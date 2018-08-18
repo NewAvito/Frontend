a764b43be5825cec595f08deed424a3f4f5dcb1e
montecamo <merrychristek@gmail.com>
2018-08-18 12:11:13 +0300

    add firebase

---
+++ src/js/containers/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+var config = {
+  apiKey: 'AIzaSyDwUI9h2lZj9xrMFmXmNJYj4S7OEnCTcTA',
+  authDomain: 'avitos-007.firebaseapp.com',
+  databaseURL: 'https://avitos-007.firebaseio.com/',
+  storageBucket: 'gs://avitos-007.appspot.com',
+};
+
+export default firebase.initializeApp(config);
